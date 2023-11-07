ccd40b58c97662a3143b0069574be7ac168d33d4
Horclot <149077334+Horclot@users.noreply.github.com>
2023-11-07 21:58:54 +0300

    I created an authorization window, made a transition between the authorization and registration windows. Didn't connect the database

--- AppGeniusHub project/AppGeniusHub/app/src/main/java/com/example/appgeniushub/LoginActivity.kt
+++ AppGeniusHub project/AppGeniusHub/app/src/main/java/com/example/appgeniushub/LoginActivity.kt
@@ -1,30 +1,28 @@
 package com.example.appgeniushub
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 
-class RegistrationActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+        setContentView(R.layout.activity_login)
 
-        val button: Button = findViewById(R.id.buttonContinue)
-        val textButton: TextView = findViewById(R.id.registrationText)
-        val name: EditText = findViewById(R.id.RegName)
-        val email: EditText = findViewById(R.id.RegEmail)
-        val password: EditText = findViewById(R.id.RegPassword)
+        val button : Button = findViewById(R.id.buttonContinue)
+        val textButton : TextView = findViewById(R.id.loginText)
+        val email: EditText = findViewById(R.id.LoginEmail)
+        val password: EditText = findViewById(R.id.LoginPassword)
 
         button.setOnClickListener {
             val emailText = email.text.toString().trim()
 
-            if (name.text.toString().isNotEmpty() &&
-                emailText.isNotEmpty() &&
+            if (emailText.isNotEmpty() &&
                 password.text.toString().isNotEmpty() &&
                 (emailText.endsWith("@gmail.com") ||
                         emailText.endsWith("@mail.ru") ||
@@ -34,7 +32,7 @@ class RegistrationActivity : AppCompatActivity() {
                         emailText.endsWith("@list.ru")
                         )
             ) {
-                val intent = Intent(this, LoginActivity::class.java)
+                val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 finish()
             } else {
@@ -43,8 +41,8 @@ class RegistrationActivity : AppCompatActivity() {
         }
     }
 
-    fun onTextClick(view: View) {
-        val intent = Intent(this, LoginActivity::class.java)
+    fun onTextClick1(view: View) {
+        val intent = Intent(this, RegistrationActivity::class.java)
         startActivity(intent)
         finish()
     }
