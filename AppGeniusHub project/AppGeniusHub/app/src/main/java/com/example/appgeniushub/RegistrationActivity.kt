4134ac22f90fff617abda34a29262ed52ed4748d
Horclot <149077334+Horclot@users.noreply.github.com>
2023-11-07 14:25:48 +0300

    Add a check for input data in the registration window

--- AppGeniusHub project/AppGeniusHub/app/src/main/java/com/example/appgeniushub/RegistrationActivity.kt
+++ AppGeniusHub project/AppGeniusHub/app/src/main/java/com/example/appgeniushub/RegistrationActivity.kt
@@ -3,8 +3,9 @@ package com.example.appgeniushub
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.provider.ContactsContract.CommonDataKinds.Email
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,17 +14,38 @@ class RegistrationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
-        var button : Button = findViewById(R.id.buttonContinue)
-        var textbutton : TextView = findViewById(R.id.loginText)
+        val button : Button = findViewById(R.id.buttonContinue)
+        val textButton : TextView = findViewById(R.id.loginText)
+        val name: EditText = findViewById(R.id.RegName)
+        val email: EditText = findViewById(R.id.RegEmail)
+        val password: EditText = findViewById(R.id.RegPassword)
 
         button.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish() // Закрыть активити регистрации
+
+            val emailText = email.text.toString().trim() // Удалить начальные и конечные пробелы
+
+            if (name.text.toString().isNotEmpty() &&
+                emailText.isNotEmpty() &&
+                password.text.toString().isNotEmpty() &&
+                (emailText.endsWith("@gmail.com") ||
+                        emailText.endsWith("@mail.ru") ||
+                        emailText.endsWith("@internet.ru") ||
+                        emailText.endsWith("@bk.ru") ||
+                        emailText.endsWith("@inbox.ru") ||
+                        emailText.endsWith("@list.ru"))) {
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish() // Закрыть активити регистрации
+
+            } else {
+                Toast.makeText(this, "Неверный формат данных", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 
-    fun onTextClick(view: View)
+    fun onTextClick()
     {
         val intent = Intent(this, MainActivity::class.java) //переход на окно навигации(ИСПРАВИТЬ!!!!)
         startActivity(intent)
