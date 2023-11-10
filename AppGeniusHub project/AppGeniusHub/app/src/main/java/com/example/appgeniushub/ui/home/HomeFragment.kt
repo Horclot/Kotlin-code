f0729bcd029d673b98d8a2b3d9db953882c10282
Horclot <149077334+Horclot@users.noreply.github.com>
2023-11-10 23:51:46 +0300

    Returned the previous version of the application

--- AppGeniusHub project/AppGeniusHub/app/src/main/java/com/example/appgeniushub/ui/home/HomeFragment.kt
+++ AppGeniusHub project/AppGeniusHub/app/src/main/java/com/example/appgeniushub/ui/home/HomeFragment.kt
@@ -1,35 +1,31 @@
 package com.example.appgeniushub.ui.home
 
-import Data.UserData
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.appgeniushub.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        // Получение данных из Bundle
-        val userData: UserData? = arguments?.getSerializable("userData") as? UserData
-
-        // Проверяем, что userData не равен null
-        if (userData != null) {
-            binding.nname.text = userData.Uname
-        }
-
         return root
     }
 
